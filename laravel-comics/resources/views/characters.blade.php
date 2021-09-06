b84c2fa4eec3a1c545c66d455d0caa04ae1eec1f
Gianluca <gl.galassini@gmail.com>
2021-09-06 20:27:35 +0200

    creo primi partials e views

---
+++ laravel-comics/resources/views/characters.blade.php
@@ -0,0 +1,6 @@
+@extends("layouts.app")
+@section("title","DC - Characters")
+@section("content")
+    <h1>DC Characters</h1>
+    <p>I personaggi della DC sono troppi per elencarli tutti, torna sulla pagina "Comics"</p>
+@endsection
