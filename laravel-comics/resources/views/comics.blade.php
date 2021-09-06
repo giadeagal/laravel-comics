cc1b71a9643bc21b35ccd58d6cc96d7ea6ef7f73
Gianluca <gl.galassini@gmail.com>
2021-09-07 00:00:17 +0200

    visualizzazione comics con foreach

--- laravel-comics/resources/views/comics.blade.php
+++ laravel-comics/resources/views/comics.blade.php
@@ -1,6 +1,17 @@
 @extends("layouts.app")
 @section("title","DC - Comics")
 @section("content")
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet nobis et voluptas commodi iste voluptate soluta dolor sunt libero maiores asperiores nostrum, praesentium laudantium sit id. A minus reiciendis vitae?</p>
-    @dd($comics)
+
+    <div class="container">
+
+        <div class="card">
+
+            @foreach($comics as $comic)
+            
+            <h3>{{ $comic['title'] }}</h3>
+            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" title="{{ $comic['title'] }}">
+            
+            @endforeach
+        </div>
+    </div>
 @endsection
