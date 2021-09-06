0d8689ab48ff87254edc4a5811a33f2fed10d15f
Gianluca <gl.galassini@gmail.com>
2021-09-06 20:38:55 +0200

    header e footer

---
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<header>
+    <div class="top">
+        header - sezione top
+    </div>
+
+    <div class="bottom">
+        <div id="logo">
+            <img src="" alt="">
+        </div>
+        
+        <nav>
+            <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
